5aa1132fb0b92b69b9bd02c18dde2d2a80117aab
oluwafemi <oo9701s@gre.ac.uk>
2024-12-13 00:12:56 +0000

    Detect tail collision

--- main.py
+++ main.py
@@ -25,19 +25,28 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.1) # slow down snake a bit
 
     snake.move()
 
     # Detect collision with snake and food
     if snake.head.distance(food) < 15:
-        scoreboard.increase_score()
-        food.refresh()
+        snake.extend() #extend snake
+        scoreboard.increase_score() #increase player score
+        food.refresh() # move the food to a random coordinate
 
     # Detect Collision with wall, then game-over
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
+
+    # Detect Collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10 :
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
